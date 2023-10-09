3c9c56ffc8daa93671a0953db6dc0377d0932a73
rurw8yr8 <145324757+rurw8yr8@users.noreply.github.com>
2023-10-09 07:55:13 +0000

    Q6

--- 4507Lab03/Q6/AdapterThreeYearsHD.java
+++ 4507Lab03/Q6/AdapterThreeYearsHD.java
@@ -1,15 +1,28 @@
 public class AdapterThreeYearsHD extends Student{
-    private double cgpa; // CGPA for 2 Years HD student
+    private double cgpa; // cgpa for 2 Years HD student
     private String pcode;
     private String pname;
 
-    public AdapterThreeYearsHD(ThreeYearsHD s) {
+    public AdapterThreeYearsHD(ThreeYearsHD s, String studentID, String studentName) {
+        super(studentName, studentID);
         this.cgpa = s.getAverageMark();
         this.pcode = s.getCcode();
         this.pname = s.getCname();
     }
 
-    public double getCgpa() {
+    public double getcgpa() {
+        if (cgpa >= 75) {
+            cgpa = 4.0;
+          } else if (cgpa >= 65) {
+            cgpa = 3.0;
+          } else if (cgpa >= 55) {
+            cgpa = 2.0;
+          } else if (cgpa >= 40) {
+            cgpa = 1.0;
+          } else {
+            cgpa = 0.0;
+          }
+      
         return cgpa;
     }
     public String getPcode() {
@@ -22,6 +35,6 @@ public class AdapterThreeYearsHD extends Student{
     public void showData(){
         System.out.println("Programme Code: " + pcode);
         System.out.println("Programme Name: " + pname);
-        System.out.println("Student's GCPA: " + cgpa);
+        System.out.println("Student's GCPA: " + getcgpa());
     }
 }
--- 4507Lab03/Q6/Lab03Q6.java
+++ 4507Lab03/Q6/Lab03Q6.java
@@ -1,9 +1,31 @@
+import java.util.Scanner;
+
 public class Lab03Q6 {
     public static void main(String[] args) {
-        Student s1 = new ThreeYearsHD(99, "19612", "HD in SE", "20123456", "AlanPo");
-        Student s2 = new TwoYearsHD(4, "IT114105", "HD in SE", "20123456", "AlanPo");
+        Scanner sc = new Scanner(System.in);
+        sc.useDelimiter("\\n");
+        String code, name, studentID, studentName;
+        int amark;
 
-        ShowResult.printResult(s1);
-        ShowResult.printResult(s2);
+        System.out.print("Input Student Name:");
+        studentName = sc.next();
+        System.out.print("Input Student ID:");
+        studentID = sc.next();
+        System.out.print("Input Three Year HD course code:");
+        code = sc.next();
+        System.out.print("Input Three Year HD course name:");
+        name = sc.next();
+        System.out.print("Input Three Year HD average Mark:");
+        amark = sc.nextInt();
+
+        Student s1 = new ThreeYearsHD(amark, code, name, studentID, studentName);
+        // Student s2 = new TwoYearsHD(4, "IT114105", "HD in SE", "20123456", "AlanPo");
+        Student s3 = new AdapterThreeYearsHD((ThreeYearsHD) s1, studentID, studentName);
+
+        // ShowResult.printResult(s1);
+        // System.out.println("");
+        // ShowResult.printResult(s2);
+        // System.out.println("");
+        ShowResult.printResult(s3);
     }
 }
